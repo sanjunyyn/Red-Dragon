a4ed27c30aa7a12f54b7b51cf3e20a4136f46b35
sanjunyyn <108185176+sanjunyyn@users.noreply.github.com>
2022-06-26 17:05:17 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,14 +23,14 @@ global.APIKeys = {
 global.alivemsg = `My name is sanju🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
+global.owner = ['+94713410878'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.ownernomer = '+94713410878' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්නsanju📍' //Costomize A Button Name In Alive Message
+global.button = '🤭මේක ටච් කරන්නch📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
